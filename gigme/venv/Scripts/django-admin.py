bb424674ca9497e8d361e8af21750744c19c0722
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2018-10-17 16:29:56 +0100

    First commit - Basic plumbing of Django to AWS database

---
+++ gigme/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\James\Desktop\DT211cYr4\FinalYearProject\gigme\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
