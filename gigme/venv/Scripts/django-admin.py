6289eac79e4a59e650fe36b35edb59317d9a9671
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2018-10-31 18:10:47 +0000

    clearing project, will restart in the next few days

--- gigme/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\James\Desktop\DT211cYr4\FinalYearProject\gigme\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
