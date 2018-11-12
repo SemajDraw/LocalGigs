ef88b656f30bf7eb38d782260437f9a15e3d48cc
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2018-11-12 20:25:13 +0000

    Basic authentication set up ono the django application, now supportindfslkjdgshflg

---
+++ Django_API/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\James\Desktop\DT211cYr4\FinalYearProject\GigMe\Django_API\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
